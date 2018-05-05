c67a37b1e71d0ab8b1f456912b869e7636c416cc
Franco Schiavinato <franco.schiavinato95@gmail.com>
2018-05-05 02:06:41 -0300

    Ya calcula la entropia de S1

--- entropia_S1.py
+++ entropia_S1.py
@@ -1,23 +1,49 @@
 from scapy.all import *
-paquetes = rdpcap('sniff_ethernet.cap')
-freq = {
-        'broadcast': {},
-        'unicast': {}
-        }
-for pkt in paquetes:
-    if pkt.getlayer(1) != None:
-        
-        # Ethernet
-        tipo = 'broadcast' if pkt[Ether].dst == 'ff:ff:ff:ff:ff:ff' else 'unicast'
-        proto = pkt.getlayer(1).name
-        
-        print(tipo)
-        print(proto)
-        # Wifi
-        #
-        if proto in freq[tipo]:
-            freq[tipo][proto] = freq[tipo][proto] + 1
-        else:
-            freq[tipo][proto] = 0
+import math
 
+paquetes = rdpcap('sniff_ethernet.cap')
+freq = {}
+paquetes = paquetes.filter(lambda pkt: pkt.getlayer(1) != None and Ether in pkt)
+for pkt in paquetes:
+    # Ethernet
+    tipo = 'broadcast' if pkt[Ether].dst == 'ff:ff:ff:ff:ff:ff' else 'unicast'
+    proto = pkt.getlayer(1).name
+    
+    # Wifi
+    #
+
+    freq[(tipo,proto)] = freq[(tipo,proto)] + 1 if (tipo,proto) in freq else 0
+
+entropia = 0
+for k in freq:
+        p = freq[k] / len(paquetes) 
+        entropia += -p * math.log(p)
+
+max_entropia = math.log(len(freq))
 print(freq)
+print(entropia)
+print(max_entropia)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
